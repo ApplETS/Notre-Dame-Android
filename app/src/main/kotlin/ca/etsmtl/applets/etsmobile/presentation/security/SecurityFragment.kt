7bccaf4c6932ec86dbd75bb5b2dcfa5f2211a5b8
JasonNarrainen <jason.narrainen.1@etsmtl.net>
2018-11-14 20:05:57 -0500

    Cleaned up the code and changed the title of SecurityFragment

--- app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/security/SecurityFragment.kt
+++ app/src/main/kotlin/ca/etsmtl/applets/etsmobile/presentation/security/SecurityFragment.kt
@@ -1,7 +1,5 @@
 package ca.etsmtl.applets.etsmobile.presentation.security
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,20 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ca.etsmtl.applets.etsmobile.R
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SecurityFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SecurityFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
+ * This fragment contains information about the security.
  */
 class SecurityFragment : Fragment() {
 
